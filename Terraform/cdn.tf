a622c03250fe84ed51a6df17b0a43a50cc915993
Mohit <mohit05054ece@gmail.com>
2022-10-05 17:09:02 +0530

    added azure  cdn

---
+++ Terraform/cdn.tf
@@ -0,0 +1,39 @@
+resource "azurerm_cdn_profile" "cdn" {
+  name                = "mohitjohri"
+  location            = "global"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard_Microsoft"
+}
+
+resource "azurerm_cdn_endpoint" "cdn_blog" {
+  name                = "mohitjohriresume"
+  profile_name        = azurerm_cdn_profile.cdn.name
+  location            = azurerm_cdn_profile.cdn.location
+  resource_group_name = azurerm_resource_group.rg.name
+  origin_host_header  = azurerm_storage_account.blog_storage.primary_web_host
+
+  origin {
+    name      = azurerm_storage_account.blog_storage.name
+    host_name = azurerm_storage_account.blog_storage.primary_web_host
+  }
+
+  tags = {
+    environment = "production"
+    purpose     = "blog"
+  }
+
+  delivery_rule {
+    name  = "EnforceHTTPS"
+    order = "1"
+
+    request_scheme_condition {
+      operator     = "Equal"
+      match_values = ["HTTP"]
+    }
+
+    url_redirect_action {
+      redirect_type = "Found"
+      protocol      = "Https"
+    }
+  }
+}
