cce72a9a66ff09a058642f5c10fd76ef1be16703
Mohit <mohit05054ece@gmail.com>
2022-10-09 00:38:39 +0530

    main.js

---
+++ StaticWebsite/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApiUrl = '';
+const localFunctionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
